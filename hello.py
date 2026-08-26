8bcd457cb9c991e0e9f5d83b2497c2123b231979
Chuichuifeng-CMYK <1424597877@qq.com>
2026-08-27 00:58:04 +0800

    练习发现语法错误

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ def greet(name):
     if name.lower() == "q":
         return "程序结束。"
     if name:
-        return f"你好，{name}！"
+        return f"你好，{name}！
     return "你好，学习者！"
 
 
@@ -19,4 +19,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    print(f"你好，{name}！
