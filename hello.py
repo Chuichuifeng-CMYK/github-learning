21c924d4c6e390b46c67e08fdaf0216793189d88
Chuichuifeng-CMYK <1424597877@qq.com>
2026-08-26 23:50:42 +0800

    实现连续问候功能，关闭 #2

--- hello.py
+++ hello.py
@@ -1,6 +1,11 @@
-print("你好，GitHub！")
-name = input("请输入你的名字：")
-if name:
-	print(f"你好，{name}！")
-else:
-	print("你好，学习者！")
+while True:
+    name = input("请输入名字（输入 q 退出）：")
+
+    if name.lower() == "q":
+        print("程序结束。")
+        break
+
+    if name:
+        print(f"你好，{name}！")
+    else:
+        print("你好，学习者！")
